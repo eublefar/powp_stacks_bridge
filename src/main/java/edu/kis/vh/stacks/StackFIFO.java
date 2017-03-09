d2af7534e619fedcddba2cb656b08b09391841f3
Mykyta Makarov <201596@edu.p.lodz.pl>
2017-03-09 15:10:42 +0100

    zrobione zadania 1,2,3 z powp-lab-kod 3.1

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -3,19 +3,20 @@ package edu.kis.vh.stacks;
 public class StackFIFO extends stack {
 
 	public stack temp = new stack();
-	
+
 	@Override
 	public int pop() {
-		while (!isEmpty())
 		
-		temp.push(super.pop());
-		
+		while (!isEmpty()) {
+			temp.push(super.pop());
+		}
+
 		int ret = temp.pop();
-		
-		while (!temp.isEmpty())
-	
-		push(temp.pop());
-		
+
+		while (!temp.isEmpty()) {
+			push(temp.pop());
+		}
+
 		return ret;
 	}
 }
