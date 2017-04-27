59dfa666d735e775e3bf74a049f39755bf006b9f
Mykyta Makarov <mykytamakarov@gmail.com>
2017-04-27 10:56:51 +0200

    z.13 Podzielilem funkcje StackDemo.main na drobniejsze

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -9,21 +9,28 @@ class StacksDemo {
 	public static void main(String[] args) {
 		DefaultStacksFactory factory = new DefaultStacksFactory();
 //		second line should start after braces
+		testStacks(factory);
+	}
+
+	static void testStacks(DefaultStacksFactory factory) {
 		Stack[] stacks = { factory.getStandardStack(), factory.getFalseStack(),
 						   factory.getFIFOStack(), factory.getHanoiStack()};
 //		no braces
-		for (int i = 1; i < 15; i++) {
-			for (int j = 0; j < 3; j++) {
-				stacks[j].push(i);
-			}
-		}
+		initStacksByRange(stacks, 15);
 //		too many tabulations
 		java.util.Random rn = new java.util.Random();
 //		braces
 		for (int i = 1; i < 15; i++) {
 			stacks[3].push(rn.nextInt(20));
 		}
-		
+		printStacks(stacks);
+//		second line should start after braces
+		System.out.println("total rejected is "
+							+ ((StackHanoi) stacks[3]).reportRejected());
+//		blank lines
+	}
+
+	static void printStacks(Stack[] stacks) {
 		for (int i = 0; i < stacks.length; i++) {
 //			braces
 			while (!stacks[i].isEmpty()) {
@@ -32,10 +39,14 @@ class StacksDemo {
 //			tabulation
 			System.out.println();
 		}
-//		second line should start after braces
-		System.out.println("total rejected is "
-							+ ((StackHanoi) stacks[3]).reportRejected());
-//		blank lines
+	}
+
+	static void initStacksByRange(Stack[] stacks, int range) {
+		for (int i = 1; i < range; i++) {
+			for (int j = 0; j < stacks.length; j++) {
+				stacks[j].push(i);
+			}
+		}
 	}
 }
 
