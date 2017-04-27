4b8388a1457f6911bf687663b76053a597a2a654
Mykyta Makarov <mykytamakarov@gmail.com>
2017-04-27 10:21:01 +0200

    Z.8 Dokonczylem enkapsulacje pol klas i Z.9 Pousuwalem niepotrzebne settery

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -1,12 +1,44 @@
 package edu.kis.vh.stacks.list;
 
 public class Node {
-
-	public int value;
-	public Node prev, next;
-
+// Stack List uses getValue(); in pop and peek functions
+// 
+//	
+	private int value; 
+//	getNext and getPrev are now used in StackList
+//	
+//	
+	private Node prev;
+	private Node next;
+//
+//	
+//	
 	public Node(int i) {
-		value = i;
+		this.value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+//This setter is not needed
+//	public void setValue(int value) { 
+//		this.value = value;
+//	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
 	}
 
 }
