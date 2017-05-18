d976679545718b1ae71ffc728037d85047e9cd06
Mykyta Makarov <mykytamakarov@gmail.com>
2017-05-18 09:44:12 +0200

    Added comment to IStack.java about reason of creating this interfaxce

--- src/main/java/edu/kis/vh/stacks/IStack.java
+++ src/main/java/edu/kis/vh/stacks/IStack.java
@@ -1,5 +1,10 @@
 package edu.kis.vh.stacks;
 
+/**
+ * @author evil_unicorn
+ * Zmiana wartości była ułatwiona przez przeniesienie do interfejsu, ale też miała nieoczywiste efekty, dlatego że implementacja stosów
+ * polegała na tej wartości, co ze swojej strony pokazuję potrzebę
+ */
 public interface IStack {
 
 	public int EMPTY_STACK_VALUE = 0;
